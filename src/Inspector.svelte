47f7f2f619c584c08ef8f53636e63d5e55b1901d
Simon Porritt <simon@jsplumbtoolkit.com>
2026-07-10 13:17:45 +1000

    updated to use new 2-way state approach with inspector

--- src/Inspector.svelte
+++ src/Inspector.svelte
@@ -4,65 +4,64 @@ import { PROPERTY_TEXT, PROPERTY_URL, TYPE_CONDITIONS, PROPERTY_NUMBER, TYPE_CAL
 import SetValuesInspectorComponent from "./SetValuesInspectorComponent.svelte"
 import {BrowserUISvelteModel, InspectorComponent, useVisuallyJsModel} from "@visuallyjs/browser-ui-svelte";
 
-let currentObj = $state<Vertex>()
-let currentType = $state('')
-
 let model = $state<BrowserUISvelteModel>()
 useVisuallyJsModel().then((s:BrowserUISvelteModel) => model = s)
 
-function renderEmptyContainer() {
-  currentType = ''
-}
 
 const portTypeMap: Record<string, string> = {
   [TYPE_CONDITIONS]: "condition"
 }
 
-function refresh(obj: Base) {
-  if (isNode(obj)) {
-    currentType = obj.type
-  } else if (isPort(obj)) {
-    currentType = portTypeMap[obj.getParent().type]
-  }
-  currentObj = obj as Vertex
+let current = $state(null)
+let currentType = $derived(deriveType(current))
+
+function deriveType(obj:Base|null):string|null {
+    if (obj == null) {
+        return null
+	}
+    if (isNode(obj)) {
+        return obj.type
+    } else if (isPort(obj)) {
+        return portTypeMap[obj.getParent().type]
+    }
+    return null
 }
 
-function updateVariables(variables: Array<any>) {
-    if (currentObj) {
-        model!.updateNode(currentObj, { variables })
+
+function updateVariables(obj:Node|null, variables: Array<any>) {
+    if (obj) {
+        model!.updateNode(obj, { variables })
         model!.clearSelection()
     }
 }
 
 function cancel() {
-    if (currentObj) {
-        model!.clearSelection()
-    }
+    model!.clearSelection()
 }
 </script>
 
-<InspectorComponent className="vjs-callflow-inspector" {refresh} {renderEmptyContainer}>
-    {#if currentType === TYPE_PLAY_AUDIO}
-      <span>Text:</span>
-      <textarea rows="10" cols="10" vjs-att={PROPERTY_TEXT} vjs-focus="true" placeholder="enter text to speak..." ></textarea>
-    {/if}
+<InspectorComponent className="vjs-callflow-inspector" bind:current={current}>
+	{#if currentType === TYPE_PLAY_AUDIO}
+		<span>Text:</span>
+		<textarea rows="10" cols="10" vjs-att={PROPERTY_TEXT} vjs-focus="true" placeholder="enter text to speak..." ></textarea>
+	{/if}
 
-    {#if currentType === TYPE_REQUEST}
-      <span>URL:</span>
-      <input type="text" vjs-att={PROPERTY_URL} vjs-focus="true" placeholder="enter request URL..." />
-    {/if}
+	{#if currentType === TYPE_REQUEST}
+		<span>URL:</span>
+		<input type="text" vjs-att={PROPERTY_URL} vjs-focus="true" placeholder="enter request URL..." />
+	{/if}
 
-    {#if currentType === TYPE_CALL_FORWARD}
-      <span>Phone Number:</span>
-      <input type="text" vjs-att={PROPERTY_NUMBER} vjs-focus="true" placeholder="enter phone number..." />
-    {/if}
+	{#if currentType === TYPE_CALL_FORWARD}
+		<span>Phone Number:</span>
+		<input type="text" vjs-att={PROPERTY_NUMBER} vjs-focus="true" placeholder="enter phone number..." />
+	{/if}
 
-    {#if currentType === TYPE_SET_VARIABLES && currentObj}
-      <SetValuesInspectorComponent obj={currentObj} onSave={updateVariables} onCancel={cancel} />
-    {/if}
+	{#if currentType === TYPE_SET_VARIABLES}
+		<SetValuesInspectorComponent obj={current} onSave={(variables) => updateVariables(current, variables)} onCancel={cancel} />
+	{/if}
 
-    {#if currentType === 'condition'}
-      <span>Condition:</span>
-      <input vjs-att="value" placeholder="enter condition..." vjs-focus="true" />
-    {/if}
+	{#if currentType === 'condition'}
+		<span>Condition:</span>
+		<input vjs-att="value" placeholder="enter condition..." vjs-focus="true" />
+	{/if}
 </InspectorComponent>
